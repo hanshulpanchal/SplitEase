de9fd4a058e10d69a7dfeb0eb5e2dc43e2f8638a
Ragib Gulzar <ragibgulzar@gmail.com>
2025-07-15 09:57:21 +0530

    Basic Project Setup

---
+++ src/main/java/com/money/EqualSplit/EqualSplitApplication.java
@@ -0,0 +1,13 @@
+package com.money.EqualSplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EqualSplitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/money/EqualSplit/EqualSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.EqualSplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualSplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
